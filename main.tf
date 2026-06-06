5daa3f2fc49e3795d1ed9e9f87b889f6408d3cd4
Carlo van Overbeek <cvanoverbeek@schubergphilis.com>
2026-06-06 21:52:08 +0200

    feat: create artifacts bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 module "mcaf-account-baseline" {
   source  = "schubergphilis/mcaf-account-baseline/aws"
   version = "7.1.0"
@@ -6,4 +9,25 @@ module "mcaf-account-baseline" {
   aws_ebs_snapshot_block_public_access_state = null
   enable_additional_eu_regions               = false
   extra_regions_to_baseline                  = []
+}
+
+module "artifacts_bucket" {
+  source  = "schubergphilis/mcaf-s3/aws"
+  version = "3.0.0"
+
+  name = format("artifacts-%s-%s", data.aws_caller_identity.current.account_id, data.aws_region.current.region)
+
+  lifecycle_rule = [
+    {
+      id      = "cleanup"
+      enabled = true
+      abort_incomplete_multipart_upload = {
+        days_after_initiation = 2
+      }
+
+      expiration = {
+        expired_object_delete_marker = true
+      }
+    }
+  ]
 }
